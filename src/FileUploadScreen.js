4e1191bd1df51434fd3d79cf4e16e5ab8e24b3eb
Deepak Tiwari <31881670+sohgaura@users.noreply.github.com>
2025-01-02 16:10:55 +0530

    Update FileUploadScreen.js
    Signed-off-by: Deepak Tiwari <31881670+sohgaura@users.noreply.github.com>

--- src/FileUploadScreen.js
+++ src/FileUploadScreen.js
@@ -1,4 +1,5 @@
 // import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 //import { PDFViewer } from 'react-pdf-viewer';
 // import 'react-pdf-viewer/styles/index.css';
 const { GoogleGenerativeAI } = require("@google/generative-ai");
